69be563d6821f233b489c77742da56616de6fae5
nik fed <nikfed142@gmail.com>
2019-04-27 19:55:21 +0300

    Add test chart data for Wpf app

---
+++ WeatherStation.Wpf/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.Windows.Input;
+using System.Windows.Media;
+using WeatherStation.Storage;
+
+namespace WeatherStation.Wpf
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly ISensorDataRepository repository;
+
+        private Brush fillColor = Brushes.Red;
+
+        public Brush FillColor
+        {
+            get => fillColor;
+            set
+            {
+                fillColor = value;
+                OnPropertyChanged(nameof(FillColor));
+            }
+        }
+
+        public SeriesCollection SeriesCollection { get; }
+
+        public ICommand ToggleDataUpdateCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            var repository = new SensorDataSqliteRepository("%homepath%/sensors.db");
+            repository.Initilize();
+
+            this.repository = repository;
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Humidity",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,7 }
+                },
+                new LineSeries
+                {
+                    Title = "Temperature",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 }
+                }
+            };
+
+            ToggleDataUpdateCommand = new RelayCommand(ToggleDataUpdate);
+        }
+
+        private void ToggleDataUpdate()
+        {
+        }
+    }
+}
