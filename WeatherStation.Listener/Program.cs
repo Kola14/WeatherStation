90c016f184cbde2b5f5cf34b28afa2256b8f9b29
nik fed <nikfed142@gmail.com>
2019-04-27 18:25:47 +0300

    Move port selection to separate method

--- WeatherStation.Listener/Program.cs
+++ WeatherStation.Listener/Program.cs
@@ -6,10 +6,25 @@ namespace WeatherStation.Listener
 {
     static class Program
     {
-        //todo: Выделить в отдельный класс? Bootstrap?
         public static void Main(string[] args)
         {
-            //todo: Слишком много всего
+            string portName = GetPortName();
+
+            var repository = new SensorDataRepository("sensors.db");
+            repository.Initilize();
+
+            var listener = new SerialPortListener(portName, 9600);
+
+            var bootrapper = new Bootstrapper(listener, repository);
+            bootrapper.Start();
+
+            Console.ReadLine();
+
+            bootrapper.Stop();
+        }
+
+        private static string GetPortName()
+        {
             Console.WriteLine("Available ports: ");
 
             var availablePorts = SerialPort.GetPortNames();
@@ -19,24 +34,17 @@ namespace WeatherStation.Listener
                 Console.WriteLine($"{i}. {availablePorts[i]}");
             }
 
-            string indexText = Console.ReadLine();
-
-            if (!int.TryParse(indexText, out int index))
+            while (true)
             {
-                return;
+                string indexText = Console.ReadLine();
+
+                if (int.TryParse(indexText, out int index) && index < availablePorts.Length)
+                {
+                    return availablePorts[index];
+                }
+
+                Console.WriteLine("Incorrect port number");
             }
-
-            var repository = new SensorDataRepository("sensors.db");
-            repository.Initilize();
-
-            var listener = new SerialPortListener(availablePorts[index], 9600);
-
-            var bootrapper = new Bootstrapper(listener, repository);
-            bootrapper.Start();
-
-            Console.ReadLine();
-
-            bootrapper.Stop();
         }
     }
 }
